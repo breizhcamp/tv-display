30afef2ddcff8f79e564eff23b1135289d7b6711
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2019-03-16 22:32:06 +0100

    Exec to chromium

--- local/show.sh
+++ local/show.sh
@@ -5,5 +5,5 @@ HERE=$(dirname "$(readlink -f "$0")")
 cd "$HERE/.."
 devilspie2 -d -f "local/devilspie2" &
 python3 -m http.server 8081 &
-chromium-browser --start-fullscreen http://localhost:8081
+exec chromium-browser --start-fullscreen http://localhost:8081
 
