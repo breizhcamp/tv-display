8d46dc7a467941bd34d5692d0caf85ea2eff95c2
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2019-03-17 13:45:55 +0100

    chromium-browser on Ubuntu...

--- local/show.sh
+++ local/show.sh
@@ -4,5 +4,5 @@ HERE=$(dirname "$(readlink -f "$0")")
 
 cd "$HERE/.."
 devilspie2 -d -f "local/devilspie2" &
-exec chromium --disable-gpu --allow-file-access-from-files --start-fullscreen --password-store=basic --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null index.html
+exec chromium-browser --disable-gpu --allow-file-access-from-files --start-fullscreen --password-store=basic --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null index.html
 
